dfb628b83c4e85e95757e81f4abf9508f29d8c4e
Md Istiak Ahmed <istinishat@gmail.com>
2024-07-09 05:10:15 +0600

    first commit

---
+++ app/muslim/baby-name/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MuslimBabyName = () => {
+  return <div>MuslimBabyName</div>;
+};
+
+export default MuslimBabyName;
---
+++ components/navbar/nav.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarContent,
+  NavbarItem,
+  Link,
+} from "@nextui-org/react";
+
+import { AcmeLogo } from "./AcmeLogo";
+
+const NavComponent = () => {
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar disableAnimation isBordered>
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <AcmeLogo />
+          <p className="font-bold text-inherit">BABY NAMES</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+          <AcmeLogo />
+          <p className="font-bold text-inherit">BABY NAMES</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent justify="end" className="hidden sm:flex">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Country
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#" aria-current="page" color="foreground">
+            Religious
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Top 100
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2
+                  ? "warning"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+};
+
+export default NavComponent;
