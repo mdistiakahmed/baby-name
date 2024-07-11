9bfc53186e8ba7d014373e8e7971763124cc9712
Md Istiak Ahmed <istinishat@gmail.com>
2024-07-11 16:54:56 +0600

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -56,6 +56,34 @@ const religions = [
   },
 ];
 
+const counties = [
+  {
+    name: "Bangladesh",
+    code: "bd",
+    desc: "Bangladeshi names",
+  },
+  {
+    name: "India",
+    code: "in",
+    desc: "Indian names",
+  },
+  {
+    name: "Japan",
+    code: "jp",
+    desc: "Japanese names",
+  },
+  {
+    name: "Korea",
+    code: "kr",
+    desc: "Korean names",
+  },
+  {
+    name: "USA",
+    code: "us",
+    desc: "American names",
+  },
+];
+
 export default function Home() {
   const currentYear = new Date().getFullYear();
   const religionSection = (
@@ -90,6 +118,58 @@ export default function Home() {
             </Link>
           );
         })}
+      </div>
+      <div className="flex items-center justify-end p-5 mr-[60px] md:mr-[80px]">
+        <Link
+          href={`/religion`}
+          className="bg-[#f06246] hover:bg-[#f8c194] hover:text-black p-2 px-5 text-white font-bold relative custom-botton"
+        >
+          See More
+        </Link>
+      </div>
+    </div>
+  );
+
+  const countrySection = (
+    <div className="flex flex-col items-center justify-center">
+      <h3 className="text-xl font-semibold">Find Name by Country</h3>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-[40px] cursor-pointer">
+        {counties.map((c, index) => {
+          const mobileBgClass =
+            index % 2 === 0
+              ? "bg-[#f06246] hover:bg-[#f3917d]"
+              : "bg-[#006fee] hover:bg-[#88b3e3]";
+          const mediumBgClass =
+            index % 2 === 0
+              ? "bg-[#f06246] hover:bg-[#f3917d]"
+              : "bg-[#006fee] hover:bg-[#88b3e3]";
+          return (
+            <Link href={`#`} key={index}>
+              <div
+                className={`h-[250px] w-[300px] rounded-xl flex flex-col items-center justify-center text-white hover:text-black ${mediumBgClass}`}
+              >
+                <Image
+                  alt={""}
+                  height={20}
+                  width={20}
+                  className="w-auto h-20"
+                  src={`https://flagcdn.com/${c.code}.svg`}
+                />
+                <p className="text-2xl text-center font-semibold">
+                  Find {c.desc}
+                </p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="flex items-center justify-end p-5 mr-[60px] md:mr-[80px]">
+        <Link
+          href={`#`}
+          className="bg-[#f06246] hover:bg-[#f8c194] hover:text-black p-2 px-5 text-white font-bold relative custom-botton"
+        >
+          See More
+        </Link>
       </div>
     </div>
   );
@@ -154,7 +234,9 @@ export default function Home() {
             </Link>
           </div>
         </div>
-        {religionSection}
+        <div>{religionSection}</div>
+
+        <div className="mt-10">{countrySection}</div>
       </div>
     </div>
   );
