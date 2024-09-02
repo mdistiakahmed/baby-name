e8847e7a9c0d38e5080cd4c04e63dc3fbf6fb108
Md Istiak Ahmed <istinishat@gmail.com>
2024-09-02 09:02:11 +0600

    Update name details page

--- app/name-details/[id]/page.tsx
+++ app/name-details/[id]/page.tsx
@@ -1,16 +1,20 @@
 import NameDetailsCard from "@/components/name-details-card/NameDetailsCard";
+import ShareWidget from "@/components/share/ShareWidget";
 import { ITEMS_PER_PAGE } from "@/utils/constants";
-import { decodeNameIndex } from "@/utils/converters";
+import { decodeNameIndex, encodeNameIndex } from "@/utils/converters";
 import { getDataUpdated } from "@/utils/getData";
-import React from "react";
+import { Metadata } from "next";
 
-const NameDetails = async ({ params }: any) => {
+export async function generateMetadata({
+  params,
+}: any): Promise<Metadata | undefined> {
   const { id } = params;
   const encodedId = id.split("-")[1];
   const { country, religion, gender, pageNumber, index } =
     decodeNameIndex(encodedId);
 
   let paginatedNameList: any = [];
+  let nameDetailsObject: any = {};
 
   if (!country && !religion) {
     const { nameList } = await getDataUpdated(null, null, gender);
@@ -19,12 +23,124 @@ const NameDetails = async ({ params }: any) => {
       (pageNumber - 1) * ITEMS_PER_PAGE,
       pageNumber * ITEMS_PER_PAGE
     );
+
+    nameDetailsObject = paginatedNameList[index];
+  }
+
+  return {
+    title: `${nameDetailsObject.name} - Meaning, Origin, and Description | BabyNameNestlings.com`,
+    description: `Discover the meaning, origin, and detailed description of the baby name ${nameDetailsObject.name}. Learn about the cultural significance and popularity of ${nameDetailsObject.name} for your little one.`,
+    openGraph: {
+      title: `${nameDetailsObject.name} - Meaning, Origin, and Description | BabyNameNestlings.com`,
+      description: `Discover the meaning, origin, and detailed description of the baby name ${nameDetailsObject.name}. Learn about the cultural significance and popularity of ${nameDetailsObject.name} for your little one.`,
+      type: "article",
+      locale: "en_US",
+      url: `http://babynamenestlings.com/name-details/${id}`,
+      siteName: "BabyNameNestlings",
+      images: [
+        {
+          url: "/baby.webp",
+          width: 1200,
+          height: 630,
+          alt: "Smiling babies",
+        },
+      ],
+    },
+  };
+}
+
+const NameDetails = async ({ params }: any) => {
+  const { id } = params;
+  const encodedId = id.split("-")[1];
+  const { country, religion, gender, pageNumber, index } =
+    decodeNameIndex(encodedId);
+
+  let paginatedNameList: any = [];
+  let nameDetailsObject: any = {};
+  const similarNames = [];
+
+  if (!country && !religion) {
+    const { nameList } = await getDataUpdated(null, null, gender);
+
+    paginatedNameList = nameList?.slice(
+      (pageNumber - 1) * ITEMS_PER_PAGE,
+      pageNumber * ITEMS_PER_PAGE
+    );
+
+    nameDetailsObject = paginatedNameList[index];
+    nameDetailsObject.gender = gender;
+    nameDetailsObject.code = id;
+
+    const actualItemPosition = (pageNumber - 1) * ITEMS_PER_PAGE + index;
+    let currentStart = actualItemPosition - 1;
+    let totalSimilarTake = 0;
+
+    while (totalSimilarTake < 10 && nameList && currentStart > 0) {
+      const updatedPageNumber = Math.floor(currentStart / ITEMS_PER_PAGE) + 1;
+      const updatedPageIndex = currentStart % ITEMS_PER_PAGE;
+
+      const id = encodeNameIndex(
+        country,
+        religion,
+        gender,
+        updatedPageNumber,
+        updatedPageIndex
+      );
+      similarNames.push({ ...nameList[currentStart], id });
+      currentStart--;
+      totalSimilarTake++;
+    }
+
+    currentStart = actualItemPosition + 1;
+
+    while (
+      totalSimilarTake < 10 &&
+      nameList &&
+      currentStart < nameList.length
+    ) {
+      const updatedPageNumber = Math.floor(currentStart / ITEMS_PER_PAGE) + 1;
+      const updatedPageIndex = currentStart % ITEMS_PER_PAGE;
+      const id = encodeNameIndex(
+        country,
+        religion,
+        gender,
+        updatedPageNumber,
+        updatedPageIndex
+      );
+      similarNames.push({ ...nameList[currentStart], id });
+
+      currentStart++;
+      totalSimilarTake++;
+    }
   }
 
   return (
-    <div className="flex items-center justify-center w-full py-10  bg-gray-100">
-      <div className="w-[95vw] md:w-[70vw] bg-white rounded-lg shadow-lg p-6">
-        <NameDetailsCard params={paginatedNameList[index]} gender={gender} />
+    <div className="flex items-center justify-center w-full  bg-gray-100">
+      <div className="w-[95vw] md:w-[70vw] p-2 md:p-6">
+        <h1 className="text-2xl font-bold text-center py-2">
+          {nameDetailsObject.name} - Meaning, Origin, and Description
+        </h1>
+        <p className="text-xl text-center py-4">
+          Discover the meaning, origin, and detailed description of the baby
+          name {nameDetailsObject.name}. Learn about the cultural significance
+          and popularity of {nameDetailsObject.name} for your little one.
+        </p>
+        <NameDetailsCard {...nameDetailsObject} gender={gender} />
+        <div className="flex items-center gap-4 p-2 justify-end">
+          <p className="font-medium">Share on</p>
+          <ShareWidget />
+        </div>
+
+        <h2 className="text-2xl font-bold py-10 underline">Similar Names</h2>
+
+        <div className="flex flex-col md:flex-row gap-5 overflow-x-auto p-2">
+          {similarNames.map((s, index) => (
+            <div key={index} className="min-w-[300px] gap-4">
+              {/* Use min-w instead of w to allow cards to be scrollable */}
+              <NameDetailsCard {...s} gender={gender} short={true} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- components/name-details-card/NameDetailsCard.tsx
+++ components/name-details-card/NameDetailsCard.tsx
@@ -1,33 +1,33 @@
 import { capitalize } from "@/utils/converters";
-import React from "react";
+import Link from "next/link";
 
-const NameDetailsCard = ({ params, gender }: any) => {
-  console.log(params);
+const NameDetailsCard = ({
+  name,
+  meaning,
+  origin,
+  gender,
+  stories,
+  short,
+  id,
+}: any) => {
   return (
-    <div>
-      {/* Card Header */}
+    <div className="bg-white rounded-lg shadow-lg p-10">
       <div className="flex items-center mb-4">
-        {/* Image Section */}
         <img
-          src="/girl_avatar.svg" // Replace with the actual image path
+          src={gender === "boy" ? "/boy_avatar.svg" : "/girl_avatar.svg"}
           alt="Name Image"
           className="w-20 h-20 rounded-full border-4 border-gray-200"
         />
-        {/* Name Section */}
         <div className="ml-4">
-          <h2 className="text-3xl font-semibold text-gray-800">
-            {params.name}
-          </h2>
+          <h2 className="text-3xl font-semibold text-gray-800">{name}</h2>
         </div>
       </div>
 
-      {/* Divider */}
       <hr className="my-4 border-gray-300" />
-      {/* Meaning Section */}
       <div className="mb-4  flex justify-between">
         <div>
           <h3 className="text-xl font-medium text-gray-700">Gender</h3>
-          <p className="text-gray-600 mt-1">{capitalize(gender)}</p>
+          <p className="text-gray-600 mt-1">{capitalize(gender || "")}</p>
         </div>
         <div className="hidden">
           <h3 className="text-xl font-medium text-gray-700">Origin</h3>
@@ -35,32 +35,38 @@ const NameDetailsCard = ({ params, gender }: any) => {
         </div>
       </div>
 
-      {/* Divider */}
       <hr className="my-4 border-gray-300" />
 
-      {/* Meaning Section */}
       <div className="mb-4">
         <h3 className="text-xl font-medium text-gray-700">Meaning</h3>
-        <p className="text-gray-600 mt-1">{params.meaning}</p>
+        <p className="text-gray-600 mt-1">{meaning}</p>
       </div>
 
-      {/* Divider */}
       <hr className="my-4 border-gray-300" />
 
-      {/* Additional Remarks Section */}
-      <div>
+      <div hidden={short}>
         <h3 className="text-xl font-medium text-gray-700">
           Additional Remarks
         </h3>
-        <ul>
-          {params.stories.map((s: any, index: any) => (
+        <ul className="list-disc">
+          {stories.map((s: any, index: any) => (
             <li key={index} className="text-gray-600 mt-1">
               {s}
             </li>
           ))}
-          <li></li>
         </ul>
       </div>
+      {short ? (
+        <div hidden={!short} className="flex items-center justify-center">
+          <Link
+            href={`/name-details/${name.toLowerCase()}-${id}`}
+            target="_blank"
+            className="bg-[#006fee] text-white font-bold p-2 px-4 rounded-lg cursor-pointer"
+          >
+            View Details
+          </Link>
+        </div>
+      ) : null}
     </div>
   );
 };
