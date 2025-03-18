5da13d03d2e401cf575ad72903673669c663f945
Md Istiak Ahmed <istinishat@gmail.com>
2025-03-18 08:22:59 +0600

    google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,24 +7,24 @@ import Breadcrumb from "@/components/breadcrumbs/Breadcrumb";
 import Footer from "@/components/footer/Footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import YouMayLIkeSection from "@/components/you-may-like/YouMayLIkeSection";
+import GoogleAdWithSuspense from "@/components/google-ads/GoogleAd";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Find the Perfect Baby Name | BabyNameNestlings",
-  description:
-    `Discover thousands of meaningful baby names across cultures, religions, and countries. Find the perfect name that reflects your heritage and values.`,
+  description: `Discover thousands of meaningful baby names across cultures, religions, and countries. Find the perfect name that reflects your heritage and values.`,
   other: {
     "google-site-verification": "cm72aHj1hmDL3KsA8DhR_aZDEngjkhWG8tPokxyCK54",
   },
   keywords: [
-    "baby names", 
-    "name finder", 
-    "cultural names", 
-    "religious names", 
-    "baby naming", 
-    "name meanings", 
-    "international names"
+    "baby names",
+    "name finder",
+    "cultural names",
+    "religious names",
+    "baby naming",
+    "name meanings",
+    "international names",
   ],
   openGraph: {
     title: "Find the Perfect Baby Name | BabyNameNestlings",
@@ -48,11 +48,11 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1
-    }
-  }
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -63,7 +63,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <meta name="google-adsense-account" content="ca-pub-9516918341435316" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#ffffff" />
@@ -73,13 +72,45 @@ export default function RootLayout({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9516918341435316"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <GoogleAnalytics gaId="G-P2GM4FBYCH" />
       <body className={`${inter.className}  bg-[url('/snowflakes.png')]`}>
         <NextUIProvider>
           <NavComponent />
           <Breadcrumb />
-          <div>{children}</div>
+          <div className="flex justify-between">
+            <div className="hidden md:block w-1/6 pt-36">
+              <GoogleAdWithSuspense>
+                <ins
+                  className="adsbygoogle"
+                  style={{ display: "block" }}
+                  data-ad-client="ca-pub-9516918341435316"
+                  data-ad-slot="5795252154"
+                  data-ad-format="auto"
+                  data-full-width-responsive="true"
+                ></ins>
+              </GoogleAdWithSuspense>
+            </div>
+            <main className="flex-1 md:mx-0">{children}</main>
+            <div className="hidden md:block w-1/6 pt-36">
+              <GoogleAdWithSuspense>
+                <ins
+                  className="adsbygoogle"
+                  style={{ display: "block" }}
+                  data-ad-client="ca-pub-9516918341435316"
+                  data-ad-slot="9056674468"
+                  data-ad-format="auto"
+                  data-full-width-responsive="true"
+                ></ins>
+              </GoogleAdWithSuspense>
+            </div>
+          </div>
           <YouMayLIkeSection />
           <Footer />
         </NextUIProvider>
